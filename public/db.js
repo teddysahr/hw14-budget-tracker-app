87177991124abd224222463ea352b458c06f86a1
Teddy Sahr <tedward1218@gmail.com>
2021-12-12 21:20:14 -0500

    added request.onsuccess function to db.js file

--- public/db.js
+++ public/db.js
@@ -11,3 +11,14 @@ request.onupgradeneeded = (event) => {
   });
   budgetStore.createIndex("transactionIndex", "transaction");
 };
+
+//When a user comes to the page, check if they are online.
+//If so, run checkDatabase which grab what's in indexedDB and sends it back to the server.
+
+request.onsuccess = function (event) {
+  db = event.target.result;
+
+  if (navigator.onLine) {
+    checkDatabase();
+  }
+};
